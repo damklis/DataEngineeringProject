33bb52b5f589bc6a7e33a6d2819c090f8312d29d
damklis <givemelajk@vp.pl>
2020-09-20 22:19:22 +0200

    Add wait in tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,7 @@
 
 docker-compose run airflow sh -c "python -m pytest -v --show-capture=no" \
 && docker-compose up -d mongo \
+&& sleep 3 \
 && docker exec -it mongo /usr/local/bin/init.sh \
 && docker-compose run api sh -c "./manage.py test -k" \
 && python -m flake8 -v 
