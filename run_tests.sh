d65693054db7a07dc76ef77a5da9aab4a92b2c5b
damklis <givemelajk@vp.pl>
2020-09-20 22:08:53 +0200

    Change in tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 docker-compose run airflow sh -c "python -m pytest -v --show-capture=no" \
-&& python -m flake8 -v \
-&& docker-compose run api sh -c "./manage.py test -k"
+&& docker-compose up -d mongo \
+&& docker exec -it mongo /usr/local/bin/init.sh \
+&& docker-compose run api sh -c "./manage.py test -k" \
+&& python -m flake8 -v 
