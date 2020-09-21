44928c761d8c91a782b2c2d581805aa3376a9bab
damklis <givemelajk@vp.pl>
2020-09-21 10:12:13 +0200

    Travis update

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+wait="sleep 15"
+
 docker-compose run airflow sh -c "python -m pytest -v --show-capture=no" \
+&& python -m flake8 -v \
 && docker-compose up -d mongo \
-&& sleep 5 \
+&& eval $wait \
 && docker exec -it mongo /usr/local/bin/init.sh \
 && docker-compose up -d api \
-&& sleep 5 \
-&& docker exec -it api ./manage.py test -k \
-&& python -m flake8 -v 
+&& eval $wait \
+&& docker exec -it api ./manage.py test -k
