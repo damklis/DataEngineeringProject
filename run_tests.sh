df0f156a0958edf4916c9a1dd9f2adfa1599b536
damklis <givemelajk@vp.pl>
2020-09-24 16:46:06 +0200

    Typo in coveralls

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 pip install -r airflow/requirements.txt \
-&& python -m py.test airflow/modules/tests/ --doctest-modules --cov airflow/modules --show-capture=no -v \
-&& python -m flake8 -v
-# && docker-compose up -d mongo \
-# && sleep 10 \
-# && docker exec -it mongo /usr/local/bin/init.sh \
-# && docker-compose up -d api \
-# && sleep 10 \
-# && docker exec -it api ./manage.py test -k \
-# && docker-compose down
+&& py.test airflow/modules/tests/ --doctest-modules --cov airflow/modules --show-capture=no -v \
+&& flake8 -v \
+&& docker-compose up -d mongo \
+&& sleep 10 \
+&& docker exec -it mongo /usr/local/bin/init.sh \
+&& docker-compose up -d api \
+&& sleep 30 \
+&& docker exec -it api ./manage.py test -k \
+&& docker-compose down
