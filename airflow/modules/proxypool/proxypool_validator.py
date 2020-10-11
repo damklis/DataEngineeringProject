57c11daf285a5ad7d5d6fe7a77886db34a0c7dd2
Mariusz Strzelecki <szczeles@gmail.com>
2020-10-11 11:28:12 +0200

    Probe proxy 5 times to ensure its stability

--- airflow/modules/proxypool/proxypool_validator.py
+++ airflow/modules/proxypool/proxypool_validator.py
@@ -1,28 +1,38 @@
 from dataclasses import dataclass
 from parser import WebParser
 from log import log
+import time
 
 
 @dataclass(frozen=True)
 class ProxyStatus:
     proxy: str
+    health: float
     is_valid: bool
 
 
 @log
 class ProxyPoolValidator:
-    def __init__(self, url, timeout=10):
+    def __init__(self, url, timeout=10, checks=5):
         self.timeout = timeout
+        self.checks = checks
         self.parser = WebParser(url, rotate_header=True)
 
     def validate_proxy(self, proxy_record):
-        content = self.parser.get_content(
-            timeout=self.timeout,
-            proxies=proxy_record.proxy
-        )
+        consecutive_checks = []
+        for i in range(self.checks):
+            content = self.parser.get_content(
+                timeout=self.timeout,
+                proxies=proxy_record.proxy
+            )
+            time.sleep(1)
+            consecutive_checks.append(int(content is not None))
+
+        health = sum(consecutive_checks) / self.checks
         proxy_status = ProxyStatus(
-            proxy_record.proxy,
-            content is not None
+            proxy=proxy_record.proxy,
+            health=health,
+            is_valid=health>=0.6 
         )
         self.logger.info(f"Proxy status: {proxy_status}")
         return proxy_status
