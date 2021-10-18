ee6ef0339dbc3e27d5c858c0f603535469f3da3a
damian.klis <damian.klis@azimo.com>
2021-10-18 13:17:04 +0200

    Upgrade images & fix bugs

--- connect/connectors/register_connectors.sh
+++ connect/connectors/register_connectors.sh
@@ -4,7 +4,7 @@
 echo -e "Waiting for Kafka Connect to start listening on localhost"
 while [ $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors) -ne 200 ] ; do 
   echo -e $(date) " Kafka Connect listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors) " (waiting for 200)"
-  sleep 5 
+  sleep 10 
 done
 echo -e $(date) "Kafka Connect is ready! Listener HTTP state: " $(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors)
 
